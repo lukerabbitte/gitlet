d809cb2c40eeb781f119da461605acf6ea1ad6f7
Luke Rabbitte <lrabbitt@tcd.ie>
2023-09-12 17:00:16 +0100

    MIME type added to Blob object

--- blob.js
+++ blob.js
@@ -2,9 +2,10 @@ const { createHash } = require('crypto');
 
 class Blob {
     
-    constructor(data) {
-      this.data = data;
-      this.id = this.hash
+    constructor(data, mimeType) {
+        this.data = data;
+        this.mimeType = mimeType;
+        this.id = this.hash(data); // Call the hash method with 'this.'
     }
 
     hash(string) {
@@ -18,4 +19,16 @@ class Blob {
     getId() {
         return this.id;
     }
+
+    getMimeType() {
+        return this.mimeType;
+    }
 }
+
+const blobData = "This is string data.";
+const mimeType = "text/plain"; // Example MIME type for text data
+const blob = new Blob(blobData, mimeType);
+
+console.log("Blob ID:", blob.getId());
+console.log("Blob Content:", blob.getContent());
+console.log("Blob MIME Type:", blob.getMimeType());
