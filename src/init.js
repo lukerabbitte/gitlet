d39e08953640e98c4e1e11875c60644561e78ce2
Luke Rabbitte <lrabbitt@tcd.ie>
2023-09-15 16:10:43 +0100

    Initialising .git folder file added

--- src/init.js
+++ src/init.js
@@ -1,1 +1,23 @@
 // TODO init must create a hidden .git folder and create objects subdirectory within. Optionally, create 16x16=256 two-digit hex prefix directories.
+
+const fs = require('fs');
+const path = require('path');
+
+function createDirectory(directoryPath) {
+    if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath);
+        console.log(`Created directory: ${directoryPath}`);
+    } else {
+        console.log(`Directory already exists: ${directoryPath}`);
+    }
+}
+
+function initGitRepository() {
+    const gitFolderPath = path.join(process.cwd(), '.git');
+    const objectsFolderPath = path.join(gitFolderPath, 'objects');
+
+    createDirectory(gitFolderPath);
+    createDirectory(objectsFolderPath);
+}
+
+initGitRepository();
