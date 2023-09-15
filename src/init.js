d0841a100364d13195d29459913dfcc1c80bdc42
Luke Rabbitte <lrabbitt@tcd.ie>
2023-09-15 16:53:41 +0100

    Changed to represent desired gitlet file structure as a JS object

--- src/init.js
+++ src/init.js
@@ -3,21 +3,34 @@
 const fs = require('fs');
 const path = require('path');
 
-function createDirectory(directoryPath) {
-    if (!fs.existsSync(directoryPath)) {
-        fs.mkdirSync(directoryPath);
-        console.log(`Created directory: ${directoryPath}`);
-    } else {
-        console.log(`Directory already exists: ${directoryPath}`);
-    }
-}
-
 function initGitRepository() {
-    const gitFolderPath = path.join(process.cwd(), '.git');
-    const objectsFolderPath = path.join(gitFolderPath, 'objects');
 
-    createDirectory(gitFolderPath);
-    createDirectory(objectsFolderPath);
+    var gitletStructure = {
+        HEAD: "ref: refs/heads/master\n",
+        objects: {},
+        refs: {
+          heads: {},
+        }
+    };
+
+    files.writeFilesFromTree(gitletStructure, process.cwd());
 }
+
+function writeFilesFromTree(tree, prefix) {
+
+    Object.keys(tree).forEach(function(name) {
+      var path = nodePath.join(prefix, name);
+      if (util.isString(tree[name])) {
+        fs.writeFileSync(path, tree[name]);
+      } else {
+        if (!fs.existsSync(path)) {
+          fs.mkdirSync(path, `0o777`); 
+        }
+
+        files.writeFilesFromTree(tree[name], path);
+      }
+    });
+
+  }
 
 initGitRepository();
