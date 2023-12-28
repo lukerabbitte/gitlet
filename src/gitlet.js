0fe0b364e80aba3aaefc1fab49b6718a6ba6a89c
lukerabbitte <luke.rabbitte@gmail.com>
2023-12-28 04:04:15 +0000

    gitCommit function implemented - needs testing

--- src/gitlet.js
+++ src/gitlet.js
@@ -3,10 +3,6 @@ const crypto = require('crypto');
 const path = require('path');
 const Blob = require('./blob.js');
 
-// Set up the index file before adding any files
-const indexFile = path.join('.gitto', 'index');
-
-// git add
 function gitAdd(filename) {
   
   // Read the file content
@@ -20,6 +16,7 @@ function gitAdd(filename) {
     fs.mkdirSync(blobDir, { recursive: true });
   }
   fs.writeFileSync(blobFile, content); // TODO no compression or serialisation of original content for now
+    // If we were to add compression or serialisation, we would need to add an extra step to decompress or deseralise when running cat-file -p.
 
   // Add the file to the index
   const indexFile = path.join('.gitto', 'index');
@@ -30,5 +27,49 @@ function gitAdd(filename) {
   }
   index[filename] = content;    // map filenames to content in index file
   fs.writeFileSync(indexFile, JSON.stringify(index));
-  
+
+}
+
+function gitCommit(message) {
+  /*
+    The git commit command takes those staged changes and creates trees pointing to all of the new blobs,
+    then creates a new commit object pointing to the new root tree. Finally, git commit also updates the
+    current branch to point to the new commit.
+  */
+
+  // TODO refactor to load tree as an object just like blob, except tree basically represents a directory
+  // rather than a file - it can reference files and also trees (subdirectories).
+
+  // Load the index
+  const indexFile = path.join('.gitto', 'index');
+  const index = JSON.parse(fs.readFileSync(indexFile, 'utf8'));
+
+  // Create a tree object for the current state of the index
+  const tree = {};
+  for (const [filename, hash] of Object.entries(index)) {
+    tree[filename] = { type: 'blob', hash };
+  }
+  const treeHash = crypto.createHash('sha1').update(JSON.stringify(tree)).digest('hex');
+  const treeDir = path.join('.gitto', 'objects', treeHash.slice(0, 2));
+  const treeFile = path.join(treeDir, treeHash.slice(2));
+  if (!fs.existsSync(treeDir)) {
+    fs.mkdirSync(treeDir, { recursive: true });
+  }
+  fs.writeFileSync(treeFile, JSON.stringify(tree));
+
+  // Create a commit object
+  const commit = { tree: treeHash, message };
+  const commitHash = crypto.createHash('sha1').update(JSON.stringify(commit)).digest('hex');
+  const commitDir = path.join('.gitto', 'objects', commitHash.slice(0, 2));
+  const commitFile = path.join(commitDir, commitHash.slice(2));
+  if (!fs.existsSync(commitDir)) {
+    fs.mkdirSync(commitDir, { recursive: true });
+  }
+  fs.writeFileSync(commitFile, JSON.stringify(commit));
+
+  // Update the current branch to point to the new commit
+  const headFile = path.join('.gitto', 'HEAD');
+  const branch = fs.readFileSync(headFile, 'utf8').split(' ')[1].trim();
+  const branchFile = path.join('.gitto', 'refs', 'heads', branch);
+  fs.writeFileSync(branchFile, commitHash);
 }
